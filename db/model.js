33ea93edcc56a86f46dd046fa42c679574d71839
Johnred Mina <johnred143.jr@gmail.com>
2022-10-01 13:52:15 +0800

    remove emailnizer to request

--- db/model.js
+++ db/model.js
@@ -41,7 +41,7 @@ const Report = new Schema(
 
 const reqform = new Schema(
   {
-    type: { type: String },
+    type: { type: String, required: true },
     name: { type: String, required: true },
     address: { type: String, required: true },
     email: { type: String, required: true },
