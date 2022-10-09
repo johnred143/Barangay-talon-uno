0559f80eb88aa65b6d653d2b9e8352fe2105cab2
User <johnred143.jr@gmail.com>
2022-10-09 14:33:48 +0800

    line 99 and email

--- controller/admin.js
+++ controller/admin.js
@@ -97,14 +97,14 @@ const reportinator = async (req, res) => {
 
     if (replog) {
       await admin({
-        to: email,
-        "request._id": ref,
+        to: user.email,
         type: "Request",
         link: "https://www.facebook.com/BrgyTalon1",
         midtext:
           "Your Request Has been Updated please contact Barangay official for more info",
         id: ref,
       });
+
       return res.json({ update: true });
     }
   }
