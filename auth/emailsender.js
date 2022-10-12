23ca1f2103552a997eb5f2521abbe0743c517e26
User <johnred143.jr@gmail.com>
2022-10-12 12:05:14 +0800

    email

--- auth/emailsender.js
+++ auth/emailsender.js
@@ -54,8 +54,7 @@ module.exports.send = async (params) => {
       <h2> ${params.type} submitted</h2>
   
       <p style="margin-bottom: 30px;">${params.midtext} </p>
-      <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;"><a href=${params.link}>Make Another ${params.type} </a></h1>
-      <h1> Your Request ID is: ${params.id} </h1>
+      <h1> <a href=${params.OTP}>Verify now</a>  </h1>
       
  </div>
      
@@ -89,7 +88,7 @@ module.exports.admin12 = async (params) => {
   
       <p style="margin-bottom: 30px;">${params.midtext} </p>
       <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;">${params.link}</h1>
-      <h1> <a href= ${params.id}>Verify now</a> </h1>
+      <h1> Your Request ID is: ${params.id} </h1>
       
  </div>
      
